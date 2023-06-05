fb46226d646b22c61326d02a6eb834230b66aa28
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-06-05 23:55:51 +0400

    Change rus

--- src/test/java/TestPositivOrderingCard.java
+++ src/test/java/TestPositivOrderingCard.java
@@ -22,7 +22,7 @@ public class TestPositivOrderingCard {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
