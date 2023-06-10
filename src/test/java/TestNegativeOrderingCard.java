90ab749f4f8c9982f14d115935c3be9a6ffa958b
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-06-10 14:31:17 +0400

    Add headless

--- src/test/java/TestNegativeOrderingCard.java
+++ src/test/java/TestNegativeOrderingCard.java
@@ -25,7 +25,7 @@ public class TestNegativeOrderingCard {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
     @AfterEach
